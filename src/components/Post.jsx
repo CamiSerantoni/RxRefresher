92ec217a3128b51f55a3f9e0d725b2c1be77535b
CamiSerantoni <camila.serantonir@gmail.com>
2025-04-17 22:03:04 -0400

    outputting dynamic values

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,13 @@
+
+const names = [ 'maximilian', 'manuel']
+
 function Post(){
     
+    const chosenName = Math.random() > 0.5 ? names[0] : names[1]
+
     return <div>
         <p>
-            Cami
+          {chosenName}
         </p>
         <p>ReactJS is awesome</p>
     </div> 
