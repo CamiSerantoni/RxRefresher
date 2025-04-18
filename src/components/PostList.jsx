71f8286d6c728e2aee9a1b87eaa5a09a366c6eca
CamiSerantoni <camila.serantonir@gmail.com>
2025-04-17 23:34:22 -0400

    adding event listeners

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,16 +1,16 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
-import classes from './PostsList.module.css'
-
-
+import classes from "./PostsList.module.css";
 
 function PostList(props) {
   return (
-    <ul className={classes.posts}>
-
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
         <Post author="maximilian" body="react js is awesome " />
         <Post author="manu" body="react js is   noooooot awesome " />
-      
-    </ul>
+      </ul>
+    </>
   );
 }
 
