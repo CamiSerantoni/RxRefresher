b3bc88a64c9d560d1e33fc71dcca437dd9e5689c
CamiSerantoni <camila.serantonir@gmail.com>
2025-04-22 23:52:01 -0400

    linking and navigating

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,16 +1,22 @@
-import classes from './Modal.module.css'
+import classes from "./Modal.module.css";
+import { useNavigate } from "react-router-dom";
 
+function Modal({ children }) {
+  const navigate = useNavigate();
+  function closeHandler() {
+    navigate("..");
+    //we use this approach to go up to the paent route
+  }
 
-function Modal({children, onClose}) {
   return (
     <>
-    <div className={classes.backdrop} onClick={onClose}/>
+      <div className={classes.backdrop} onClick={closeHandler} />
 
-    <dialog open className={classes.modal}>{children}</dialog>
-    
-    
+      <dialog open className={classes.modal}>
+        {children}
+      </dialog>
     </>
-  )
+  );
 }
 
-export default Modal
+export default Modal;
